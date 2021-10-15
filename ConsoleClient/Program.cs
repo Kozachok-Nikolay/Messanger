42175042b0a019c559b7ad493f0a1a1f7f13238d
Николай Козачок <deraeser@bk.ru>
2021-10-15 16:19:58 +0300

    fix

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-
+using Newtonsoft.Json;
 namespace Messanger
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Message msg = new Message();
-      Console.WriteLine("Begin of the end!");
-      Console.WriteLine(msg.ToString());
+      Message msg = new Message("Kolya", "Privet", DateTime.UtcNow);
+      string output = JsonConvert.SerializeObject(msg);
+      Console.WriteLine(output);
+      Message normalOutput = JsonConvert.DeserializeObject<Message>(output);
+      Console.WriteLine(normalOutput);
+      //{ "UserName":"Kolya","MessageText":"Privet","TimeStamp":"2021-10-15T13:17:03.9820482Z"}
+      //Kolya < 15.10.2021 13:17:03 >: Privet
     }
   }
 }
